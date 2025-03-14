6dad830072061a04f7af95def987a9f7957f13b7
Sayfiyev99 <asror6443634@gmail.com>
2025-03-15 03:33:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 # .env faylni yuklash
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "7759067760:AAF0KqWe3lvLt-_NiJqJF6EEiGKENPDYFUk"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 INSTAGRAM_API_URL = os.getenv("INSTAGRAM_API_URL")
 TIKTOK_API_URL = os.getenv("TIKTOK_API_URL")
